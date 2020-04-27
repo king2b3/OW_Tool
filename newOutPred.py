62dec29e0eeb67f1116754fc078f5378586781c2
Bayley King Windows <king2b3@mail.uc.edu>
2020-04-26 22:14:35 -0400

    match predictor completion
    generation of head to head matchups
    basic version of match predictor done

---
+++ newOutPred.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from os import system, name
+from time import sleep
+import timeit
+import random
+import numpy as np
+from tabulate import tabulate
+import pdfkit as pdf
+import tensorflow as tf
+from tensorflow import keras
+
+
+def clear():
+    _ = system('clear')
+
+
+RANDOM_SEED = 42
+tf.set_random_seed(RANDOM_SEED)
+
+
+def get_tranq_data():
+    '''
+        Reads in data from tranq output files for match prediction
+    '''
+    '''
+    import csv
+    dataFile = open("trainX.csv")
+    trainX = dataFile.readlines()
+    dataFile = open("trainY.csv")
+    trainY = dataFile.readlines()
+    '''
+    import pickle as pkl
+    import numpy as np
+    trainX = np.array(pkl.load( open("trainX.pkl", "rb" ) ))
+    trainY = np.array(pkl.load( open("trainY.pkl", "rb" ) ))
+
+    return trainX, trainY
+
+
+
+
+def main():
+    #train_X, test_X, train_y, test_y = get_iris_data()
+
+
+    train_X, train_y = get_tranq_data()
+
+    model = tf.keras.Sequential()
+
+
+    # Layer's sizes
+    x_size = train_X.shape[1]   # Number of input nodes: 8 features and no bias
+    h_size = 50                 # Number of hidden nodes
+    y_size = train_y.shape[1]   # Number of outcomes (team 1 wins or team 2 wins)
+
+
+    clear()
+    for epoch in range(100000):
+        # Train with each example
+        for i in range(len(train_X)):
+            sess.run(updates, feed_dict={X: train_X[i: i + 1], y: train_y[i: i + 1]})
+
+        train_accuracy = np.mean(np.argmax(train_y, axis=1) ==
+                                 sess.run(predict, feed_dict={X: train_X, y: train_y}))
+        '''
+        test_accuracy  = np.mean(np.argmax(test_y, axis=1) ==
+                                 sess.run(predict, feed_dict={X: test_X, y: test_y}))
+        '''
+
+        print("Epoch = %d, train accuracy = %.2f%%" #, test accuracy = %.2f%%"
+              % (epoch + 1, 100. * train_accuracy)) #, 100. * test_accuracy))
+    sess.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ printData.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from os import system, name
+from time import sleep
+import pdfkit as pdf
+ 
+
+def clear():
+    _ = system('clear')
+
+
+clear()
+toPrint = []
+outData = pd.read_csv("OutData.csv",dtype=object,index_col=0)
+
+team1 = "FIGHTING POTATOES"
+team1 = team1.upper()
+test1 = outData.loc[team1]
+test1 = test1.to_frame()
+
+
+team2 = "KARMA"
+team2 = team2.upper()
+test2 = outData.loc[team2]
+test2 = test2.to_frame()
+
+temp = pd.concat([test1,test2],axis=1,join='inner')
+
+print(temp)
+
+
+temp.to_html('test.html')
+
+path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+config = pdf.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
+
+pdf.from_file('test.html', 'output.pdf',configuration=config)
+#pdf.from_file('test.html', 'output.pdf')
